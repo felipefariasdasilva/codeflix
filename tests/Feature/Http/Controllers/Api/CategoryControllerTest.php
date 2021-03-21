9f323c2ade987b15ba5d65aae3f56cbb89f81a15
FELIPE FARIAS DA SILVA <FSILVA38@corp.ad.emb>
2021-03-21 14:04:37 -0300

    :hammer: refactor: changed project structure

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -206,4 +206,18 @@ class CategoryControllerTest extends TestCase
         ]);
     }
 
+    public function testDestroy(){
+        $category = factory(Category::class)->create();
+
+        $response = $this->json(
+            'DELETE',
+            route('categories.destroy', ['category' => $category->id]),
+            []
+        );
+
+        $response->assertStatus(204);
+        $this->assertNull(Category::find($category->id));
+        $this->assertNotNull(Category::withTrashed()->find($category->id));
+    }
+
 }
